52baee109ea57d4657d83a54a3be6eba337e5860
feyfath <fath5yousfi@gmail.com>
2024-11-09 02:59:00 +0100

    initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <Page>
+        <ActionBar title="Fath native App!"/>
+        <GridLayout columns="*" rows="*">
+            <Label class="message" :text="msg" col="0" row="0"/>
+            <button class="bt">hello there bt</button>
+            <label for="x">fath</label>
+            <input type="text" name="x" id="x">
+        </GridLayout>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+    .bt{
+      font-size: 10px;
+      width: 200px;
+      background: #333333;
+      color: white;
+    }
+</style>
